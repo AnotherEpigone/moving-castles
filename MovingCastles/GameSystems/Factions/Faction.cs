75368ad77c455e26cf8e39d3ebff31aa8d7301d0
AnotherEpigone <mikestrauss612@gmail.com>
2020-12-27 15:00:46 -0500

    faction wip

---
+++ MovingCastles/GameSystems/Factions/Faction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace MovingCastles.GameSystems.Factions
+{
+    public class Faction
+    {
+        private const int EnemyThreshold = -100;
+
+        public Faction(string name)
+            : this(name, new Dictionary<string, int>()) { }
+
+        public Faction(string name, Dictionary<string, int> relationships)
+        {
+            Name = name;
+            Relationships = relationships;
+        }
+
+        public string Name { get; }
+
+        private Dictionary<string, int> Relationships { get; }
+
+        public bool IsEnemy(Faction other)
+        {
+            if (!Relationships.TryGetValue(other.Name, out var otherRelationship))
+            {
+                return false;
+            }
+
+            return otherRelationship < EnemyThreshold;
+        }
+    }
+}
