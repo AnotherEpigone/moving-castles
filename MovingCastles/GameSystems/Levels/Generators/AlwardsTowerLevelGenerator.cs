9cb93d808ea3a10a93fb1902a23d2ab3ec92d26c
AnotherEpigone <mikestrauss612@gmail.com>
2021-04-30 19:07:26 -0400

    update trollshroom art + tweaked spawn chance

--- MovingCastles/GameSystems/Levels/Generators/AlwardsTowerLevelGenerator.cs
+++ MovingCastles/GameSystems/Levels/Generators/AlwardsTowerLevelGenerator.cs
@@ -263,7 +263,7 @@ namespace MovingCastles.GameSystems.Levels.Generators
             {
                 var canPlaceBlocker = PlacementRules.CanPlaceBlockingObject(pos, doors, level, room);
                 if (canPlaceBlocker
-                    && rng.Next(100) < 5)
+                    && rng.Next(1000) < 5) // 0.5%
                 {
                     level.Map.AddEntity(GameModeMaster.EntityFactory.CreateDoodad(pos, DungeonModeDoodadAtlas.Trollshroom));
                     continue;
